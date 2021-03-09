999add411560e2ae671a03b40b9275094da36db3
Sunny Gosalia <sgosalia@bhhc.com>
2021-03-09 07:45:19 -0800

    code clean-up and removed Twilio Account details

---
+++ Sparrow/Service/Sms.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Service
+{
+    public class Sms
+    {
+        public string CheckIncomingMessage(string incomingText)
+        {
+            if (incomingText.ToLower() == "hi")
+            {
+                return GetWelcome();
+            }
+            else if (incomingText.ToLower() == "claimnumber 1234")
+            {
+                return "\nClaim number: 1234\nStatus: Closed\nClosed Date: 03/07/2021";
+            }
+            else if (incomingText.ToLower() == "claimnumber 4321")
+            {
+                return "\nClaim number: 4321\nStatus: Re-opened\nRe-opened Date: 03/07/2021\nRe-open reason: Litigation";
+            }
+            else if (incomingText.ToLower() == "claimnumber 1111")
+            {
+                return "\nClaim number: 1111\nStatus: Open";
+            }
+            else
+            {
+                return GetProviderByLoction(incomingText);
+            }
+        }
+        public string GetWelcome()
+        {
+            return "\nHi! I'm SMS Bot. I can help you with:\nIn-Network Physician? REPLY <ZIPCODE>.\nClaim Status REPLY <ClaimNumber XXXX>";
+          
+        }
+        public string GetProviderByLoction(string zipCode)
+        {
+            if (zipCode == "94111")
+            {
+                return "\nProvider:Primary Treating Physician(In-Network)\nAddress:26 CALIFORNIA ST San Francisco, CA 94111\nPhone:(415) 781-7077";
+            }
+            else if (zipCode == "94538")
+            {
+                return "\nProvider:Primary Treating Physician(In-Network)\nAddress:39400 Paseo Padre Pky Fremont, CA 94538\nPhone:(510) 248-3015";
+            }
+            else if (zipCode == "95695")
+            {
+                return "\nProvider:Primary Treating Physician(In-Network)\nAddress:39400 Paseo Padre Pky Fremont, CA 94538\nPhone:(510) 248-3015";
+            }
+            else {
+                return "Invalid Entry!";
+            }
+        }
+    }
+}
--- Sparrow/Sparrow/Controllers/SmsController.cs
+++ Sparrow/Sparrow/Controllers/SmsController.cs
@@ -5,64 +5,29 @@ using Twilio.AspNet.Mvc;
 using Twilio.Rest.Api.V2010.Account;
 using Twilio.Types;
 using System.Net.Mail;
+using Service;
 
 namespace Sparrow.Controllers
 {
     public class SmsController : TwilioController
     {
-        public ActionResult SendSms()
-        {
-            var accountSid = "ACd7866d577d7850a0d4beee7722edd17e";
-            var authToken = "389a892c27cadf5c81a6b0b4b0908350";
-            TwilioClient.Init(accountSid, authToken);
-            var to = new PhoneNumber("+14157451519");
-            var from = new PhoneNumber("+19898001313");
-
-            var message = MessageResource.Create(
-                to: to,
-                from: from,
-                body: "Hi! I'm SMS Bot from BHHC. I can help you with:\nIn-Network Physician? REPLY <ZIPCODE>.\nClaim Status REPLY <CLAIMNUMBER>\nPayment Status REPLY RECENTPAYMENT"
-                );
-            return Content("Message Sent!");
-        }
-
         public ActionResult ReceiveSms()
         {
             var response = new MessagingResponse();
-            response.Message("Thank you!");
+            var requestBody = Request.Form["Body"].ToString();
+            var requester = Request.Form["From"].ToString();
+            Sms sms = new Sms();
+            var accountSid = "<AccountSIDFromYourTwilioAccount>";
+            var authToken = "<AuthTokenFromYourTwilioAccount>";
+            TwilioClient.Init(accountSid, authToken);
+            var to = new PhoneNumber(requester);
+            var from = new PhoneNumber("<YourTwilioPhoneNumber>");
+            var message = MessageResource.Create(
+                to: to,
+                from: from,
+                body: sms.CheckIncomingMessage(requestBody)
+                );
             return TwiML(response);
         }
-
-        public ActionResult smspost()
-        {
-            var requestBody = Request.Form["Body"];
-            var response = new MessagingResponse();
-            if (requestBody == "hello")
-            {
-                response.Message("Hi!");
-            }
-            else if (requestBody == "bye")
-            {
-                response.Message("Goodbye");
-            }
-
-            return TwiML(response);
-        }
-
-        public ActionResult SMSv()
-        {
-            var message = new MailMessage();
-            message.From = new MailAddress("sgosalia@bhhc.com");
-            message.To.Add(new MailAddress("5107714313@txt.att.net"));
-            message.Subject = "This is my subject";
-            message.Body = "This is the content";
-            var client = new SmtpClient();
-            client.Host = "wcsmtp.berkshire.local";
-            client.Port = 25;
-            client.UseDefaultCredentials = false;
-            client.Send(message);
-            return Content("SMS Sent!");
-        }
-
     }
 }
